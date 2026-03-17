aecd481b7923c628f9cdc0a6668a165bfa8ecb43
shraddhabute <shraddhacb2747@gmail.com>
2026-03-17 22:15:46 +0530

    Add files via upload

---
+++ Project 2- Gym Website/script.js
@@ -0,0 +1,11 @@
+function joinNow() {
+  alert("Welcome to PowerFit Gym 💪");
+}
+
+function buyPlan(plan) {
+  alert("You selected " + plan + " plan");
+}
+
+function submitForm() {
+  alert("Message sent!");
+}
